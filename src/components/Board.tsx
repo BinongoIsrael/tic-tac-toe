76b7bd742aa14c9a9ec6801eb557ec3e4defcb7b
Israel <israelmelorenbinongo@gmail.com>
2025-09-13 23:26:39 +0800

    Implement game history

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,33 +5,29 @@ import { type BoardState, type SquareValue } from "../types";
 interface BoardProps {
   squares: BoardState;
   onClick: (i: number) => void;
-  status: string;
 }
 
-const Board: React.FC<BoardProps> = ({ squares, onClick, status }) => {
+const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
   const renderSquare = (i: number) => (
     <Square value={squares[i]} onClick={() => onClick(i)} />
   );
 
   return (
-    <div>
-      <div className="status">{status}</div>
-      <div className="board">
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
