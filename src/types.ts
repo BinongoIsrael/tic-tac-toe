247339747ce214a450dc988323133919f9de90c7
Israel <israelmelorenbinongo@gmail.com>
2025-09-14 03:52:17 +0800

    Add isMiniBoard to BoardProps

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,9 @@ export type SquareValue = 'X' | 'O' | null;
 export type BoardState = SquareValue[];
 export type GameMode = 'pvp' | 'pvc'
 
+export interface BoardProps {
+  squares: BoardState;
+  onClick: (i: number) => void;
+  isMiniBoard?: boolean;
+}
+
